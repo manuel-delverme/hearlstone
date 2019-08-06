a5d472db0746d8286dfd5e2a9bd9b5fdee71fa78
d3sm0 <simone.totaro@gmail.com>
2019-08-06 22:09:44 +0200

    forgot a test

---
+++ tests/test_elo_score.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+from game_utils import GameManager, Elo
+from agents.heuristic.random_agent import RandomAgent
+import hs_config
+
+elo = Elo()
+manager = GameManager(hs_config.Environment.address)
+manager.use_heuristic_opponent = True
+env = manager()
+agent = RandomAgent()
+
+scores ={0:[]} # idx, list of rewards
+for ep in range(10):
+  o, _, _, info = env.reset()
+
+  while True:
+    a = agent.choose(o, info)
+    o, r, d, info, = env.step(a)
+    if d:
+      scores[0].append(r)
+      break
+
+elo.update(scores)
+
