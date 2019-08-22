49c614fa111b3cfa6d3fd061fb231ec86bd40abc
Manuel <manuel.delverme@gmail.com>
2019-08-22 19:41:32 +0200

    Update mmf_cleaner.sh

--- cmd/mmf_cleaner.sh
+++ cmd/mmf_cleaner.sh
@@ -1,2 +1,3 @@
 #!/usr/bin/env bash
-rm -rf *_mmf_*.txt /tmp/*tmp*.tmp /tmp/*.mmf /tmp/CoreFxPipe_sabberstoneserver_* /clr-debug-pipe-*
+rm -rf *_mmf_*.txt /tmp/*.mmf /tmp/CoreFxPipe_sabberstoneserver_* /clr-debug-pipe-*
+echo "GO DEAL WITH" /tmp/*tmp*.tmp 
